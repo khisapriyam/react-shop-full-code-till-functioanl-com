2814fa8ab0b0b12b13f97ab0fc71fb1b94bc7ddc
khhisapriyam@gmail.com <khisapriyam@gmail.com>
2022-08-21 18:54:31 +0200

    this code is till front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Pages/Home';
+import ProductSingle from './components/Pages/ProductSingle';
+import Shop from './components/Pages/Shop';
+
+import './_assets/css/bundle.css'
+import './_assets/css/style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <BrowserRouter>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={ <Home />}/>
+          <Route path="/shop" element={ <Shop />}/>
+          <Route path="/shop/:name" element={ <ProductSingle/>}/>
+        </Routes>
+        <Footer></Footer>
+      </BrowserRouter>
+      
+      
+      
+     
+    </>
   );
 }
 
